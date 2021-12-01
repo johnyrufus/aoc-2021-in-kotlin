634929d607f79e037d49e77bbf656a696af2cb5e
Johny Rufus <johnyrufus@gmail.com>
2021-11-30 23:04:09 -0800

    Solve Day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,25 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        return input.zipWithNext { a, b ->
+            when {
+                a.toInt() < b.toInt() -> 1
+                else -> 0
+            }
+        }.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val input = input.map{it.toInt()}
+        return (0 until input.size-3).map {
+            when {
+                input[it] + input[it+1] + input[it+2] < input[it+1] + input[it+2] + input[it+3] -> 1
+                else -> 0
+            }
+        }.sum()
     }
 
-    // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    println(part1(testInput))
+    println(part2(testInput))
 
-    val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
 }
